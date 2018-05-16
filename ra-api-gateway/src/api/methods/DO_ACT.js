9a87f19de41044d7434371168852140f72cf4c2a
SeregaV <serega.vetukh@gmail.com>
2018-05-16 12:07:27 +0300

    Api cleanup

--- ra-api-gateway/src/api/methods/DO_ACT.js
+++ ra-api-gateway/src/api/methods/DO_ACT.js
@@ -2,35 +2,19 @@ import log from '../../utils/log'
 
 export default async ({ payload, emitAction, db, sessionData }) => {
   let result
-  let { a } = payload
-  let { b } = payload
-  let { action } = payload
-
+  let { a, b, action } = payload
+  
   try {
-    // switch(action)
-    // {
-    //   case "*":
-    //     result = a * b;
-    //     break
-    //   case "+":
-    //     result = a + b;
-    //     break;
-    //   case "-":
-    //     if(b < 0)
-    //       {
-    //         b = b * -1
-    //         result = a + b
-          
-    //       }else{
-    //         result = a - b;
-    //       }
-    //     break
-    //   case "/":
-    //     result = a / b;
-    //     break
-    // }
-    var val = "" + a + " " + action + " " + b;
-    result = eval(val);
+    switch(action)
+    {
+      case "*":
+      case "+":
+      case "-":
+      case "/":
+        var val = "" + a + " " + action + " " + b;
+        result = eval(val);
+        break
+    }
     emitAction('RESULT_ACT', { a: a, b: b, action: action, result: result })
   } catch (error) {
     emitAction('SNACK', {
@@ -38,6 +22,4 @@ export default async ({ payload, emitAction, db, sessionData }) => {
     })
     log.err(error)
   }
-  //emitAction('SNACK', { a, b, action, result })
-  // emitAction('USERS_LIST', {usersList, 'canLoadMore':false})
 }
